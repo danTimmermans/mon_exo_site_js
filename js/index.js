fc041d7b521d2c3da747c04e2ab9613d7665a209
unknown <dantim1974@hotmail.com>
2022-09-20 14:31:18 +0200

    commit

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ window.addEventListener("scroll", () =>{
     }
     yLastPosition = window.scrollY;
 });
-
+// barre de progression
 window.onload = () => {
     // Ecouteur d'évènement sur scroll
     window.addEventListener("scroll", () => {
@@ -43,16 +43,58 @@ window.addEventListener("scroll", () =>{
     }
     yLastPosition = window.scrollY;
 });
-
+// effet pop up burger et nav
 let burger2 = document.querySelector(".burger-menu");
 let nav2 = document.querySelector(".nav_2");
 let link = document.querySelectorAll(".nav_2_link")
-console.log(link);
+// console.log(link.indexOf());
+let navLinks = [...document.querySelectorAll('.nav_2 a')];
+console.log(navLinks);
+let sections = [...document.querySelectorAll('section')];
+let sectionPosition; 
+
 function hasClick(){
+    nav2.classList.remove('inactive');
     nav2.classList.toggle("active");
-    burger2.classList.toggle("disappears")
+    burger2.classList.toggle("disappears");
 }
 for(i=0; i<link.length; i++){
-    link[i].addEventListener("click", hasClick);
+    link[i].addEventListener("click", closeNav);
+    // link[i].addEventListener('click', addScrollSmooth);
 }
 burger2.addEventListener("click", hasClick);
+
+
+nav2.addEventListener('click',hasClick);
+function closeNav(){
+    nav2.classList.toggle('inactive');
+    burger2.classList.toggle("appears");
+}
+
+// smooth-scroll
+function positionCalc(){
+    sectionPosition = sections.map(section => section.offsetTop)
+}
+positionCalc();
+console.log(sectionPosition);
+
+navLinks.forEach(navLink => navLink.addEventListener('click', addScrollSmooth));
+
+function addScrollSmooth(e){
+    let linkIndex = navLinks.indexOf(e.target);
+    console.log(sectionPosition[linkIndex]);
+    console.log('hello');
+    window.scroll({
+        top: sectionPosition[linkIndex], behavior: 'smooth'
+    })
+}
+
+
+
+
+
+// let tab = ['a', 'b,','c'];
+// let el = 'a';
+// let idx = tab.indexOf(el);
+// console.log(idx);
+
